e8b2c6b7f8be2d6cb1e1d99ffb15f0915da261c4
Roy M J <roymj88@gmail.com>
2013-12-31 18:09:56 +0530

    Update jquery.transition.js

--- jquery.transition.js
+++ jquery.transition.js
@@ -1,4 +1,5 @@
 (function($) {
+	/*General Settings*/
         var settings = {
                 effect: "fadein",
 		duration: "1000"
@@ -6,9 +7,28 @@
         
         $.fn.transit = function(options) {
                 $(this).css("display", "none");
-                if(settings.effect == 'fadein') {
+                /*Fetching user defined values*/
+                var effect   = options.effect;
+                var duration = options.duration;
+                /*Fetching config values*/
+                
+                
+                /*Checking with config values*/
+                if(!effect){
+                	effect = settings.effect;	
+                }
+                if(!duration){
+                	duration = settings.duration;	
+                }
+                /*Checking with config values*/
+                
+                
+                /*Applying corresponding effects*/
+                if(effect == 'fadein') {
 			$(this).fadeIn(duration);
 		}
+                /*Applying corresponding effects*/
+                
 		/*else if(settings.effect == 'animate'){
 			$(this).fadeIn(duration);
 		}*/
