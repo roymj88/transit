0f235d672005b6881242af997888a211ad5e2fe5
Roy M J <roymj88@gmail.com>
2013-12-31 18:05:53 +0530

    Update jquery.transition.js

--- jquery.transition.js
+++ jquery.transition.js
@@ -1,23 +1,18 @@
 (function($) {
-	
-	$.fn.transit = function(options) {
-		
-		$(this).css("display", "none"); 
-		this.doTransit();
-		
-		var settings = $.extend({
-			effect: "fadein",
-			duration: "1000",
-			}, options);
-			
-		doTransit: function() {
-			if(settings.effect == 'fadein') {
-				$(this).fadeIn(duration);
-			}else if(settings.effect == 'animate'){
-				$(this).fadeIn(duration);
-			}
+        var settings = {
+                effect: "fadein",
+		duration: "1000"
+        }; 
+        
+        $.fn.transit = function(options) {
+                $(this).css("display", "none");
+                if(settings.effect == 'fadein') {
+			$(this).fadeIn(duration);
 		}
-	};
-	
+		/*else if(settings.effect == 'animate'){
+			$(this).fadeIn(duration);
+		}*/
+        };
+        
 }( jQuery ));
 
