fcb0e99e8ccd8108896e6bb78677dbe4f74472f4
Roy M J <roymj88@gmail.com>
2013-12-31 17:51:17 +0530

    Update jquery.transition.js

--- jquery.transition.js
+++ jquery.transition.js
@@ -1,4 +1,13 @@
 (function($) {
+	var effects = {
+		an : animate,
+		fn : fadeIn,
+		sd : slide,
+		pp : pop,
+	};	
+		
+	
+	
 	$.fn.transit = function() {
 		$(this).css("display", "none"); 
 		$(this).fadeIn(2000);
