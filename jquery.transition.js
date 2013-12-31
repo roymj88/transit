fa09771bb7940d5b18378c2ba340c8385dd86c23
Roy M J <roymj88@gmail.com>
2013-12-31 17:57:52 +0530

    Update jquery.transition.js

--- jquery.transition.js
+++ jquery.transition.js
@@ -3,13 +3,13 @@
 	$.fn.transit = function(options) {
 		
 		$(this).css("display", "none"); 
-		duration = this.options.duration;
 		this.doTransit();
 		
 		var settings = $.extend({
 			effect: "fadein",
 			duration: "1000",
-			}, options),
+			}, options);
+			
 		doTransit: function() {
 			if(settings.effect == 'fadein') {
 				$(this).fadeIn(duration);
