6eec7688c503d89b371121d7cfc6d38eaacff835
Roy M J <roymj88@gmail.com>
2013-12-31 17:43:31 +0530

    Update jquery.transition.js

--- jquery.transition.js
+++ jquery.transition.js
@@ -5,14 +5,3 @@
 	};
 }( jQuery ));
 
-$(document).ready(function(){
-	$("body").transit();
-});
-
-$(document).on("click",".transit", function(){
-	$("body").transit();
-});
-
-$(document).on("click", ".div1", function(){
-	$(".transitDiv").transit();
-});
